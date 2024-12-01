5648d30356d00741226a2efe994d2637f8f3de4c
Moaid Ali Alshamrani <moaid.alshamrani@gmail.com>
2024-12-01 17:53:08 +0300

    Update picture.php

--- pages/picture.php
+++ pages/picture.php
@@ -18,22 +18,22 @@ include('../includes/header.php');
 
 <div class="square_images">
     <div class="image_items">
-        <img src="../Images/Book1.png" id="Book1"  class="images"   alt="Books Images"/>
+        <img src="../images/Book1.png" id="Book1"  class="images"   alt="Books Images"/>
         <p class="image_details">Book: The Alchemist <br /><br /> Moaid55 <br /><br />Location: Jeddah</p>
         
     </div>
     <div class="image_items">
-        <img src="../Images/Book2.png" id="Book2" class="images"  alt="Books Images"/>
+        <img src="../images/Book2.png" id="Book2" class="images"  alt="Books Images"/>
         <p class="image_details">Book: The Old Man and the Sea  <br /><br /> Ahmed55 <br /><br />Location: Jeddah</p>
         
     </div>
     <div class="image_items">
-        <img src="../Images/Book3.png" id="Book3" class="images"  alt="Books Images"/>
+        <img src="../images/Book3.png" id="Book3" class="images"  alt="Books Images"/>
         <p class="image_details"> Book: The Magic Mountain <br /><br /> Mohammed55 <br /><br />Location: Jeddah</p>
         
     </div>
     <div class="image_items">
-        <img src="../Images/Book4.png" id="Book4" class="images"  alt="Books Images"/>
+        <img src="../images/Book4.png" id="Book4" class="images"  alt="Books Images"/>
         <p class="image_details">Book: The Stranger <br /><br /> Ali123 <br /><br />Location: Jeddah</p>
         
     </div>
