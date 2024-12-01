f335df19c0d66c973dea3289f7c3c0217624050a
Moaid Ali Alshamrani <moaid.alshamrani@gmail.com>
2024-12-01 19:52:30 +0300

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -1,50 +1,8 @@
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-
-
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>LitSwap</title>
-    <link rel="stylesheet" type="text/css" href="../css/main.css" />
-    <link rel="stylesheet" href="../css/print.css" media="print"/>
-    <script src="../css/hover.js" type="text/javascript"></script>
-    <script src="../css/validation.js" type="text/javascript"></script>  
-</head>
-
-<body>
-    
-        <div class="heading" id = "header">
-            <h2><a href="../pages/home.php">LitSwap</a></h2>
-
-            
-
-            <div class="tools">
-            <p>
-
-                                
-                <a href="../pages/trading.php">Trading</a> | 
-                                <a href="../pages/login.php">Log in</a> | 
-                
-                <a href="../pages/contact.php">Contact us</a> |
-                <a href="../pages/table.php"> Table </a> |
-                <a href="../pages/picture.php"> Picture Gallery </a> |
-                <a href="../pages/video.php"> Video </a> |
-                <a href="../pages/resume.php"> Resume </a> |
-                <a href="../pages/form.php"> Feedback Form</a>
-
-            </p>
-            </div>
-
+<?php include '../includes/header.php';
+$pageTitle = " Feedback Form - Page"; ?>
+<?
         
-        </div>
-        
-
-        <hr id= "line" />
-        
-    
-
 <fieldset>
     <legend>Contact Information</legend> <!-- Use lowercase for HTML5 compliance -->
 
@@ -90,17 +48,4 @@
     </address>
 </div>
 
-<div class="footer" id ="footer">
-        <hr />
-        <p style="text-align: center;">&copy; 2024 Moaid Ali Alshamrani. All rights reserved.</p>
-
-        <a href="https://validator.w3.org/check?uri=http://localhost:3000/pages/">
-            <img src="http://www.w3.org/Icons/valid-xhtml10" alt="HTML Validation" style="height: 25px;" />
-        </a>
-
-        <a href="https://jigsaw.w3.org/css-validator/check/referer">
-            <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Validation" style="height: 25px;" />
-        </a>
-    </div>
-</body>
-</html>
+<?php include '../includes/footer.php'; ?>;
