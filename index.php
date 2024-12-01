91202f5f599aa55b873f990d303099630933ee3e
Moaid Ali Alshamrani <moaid.alshamrani@gmail.com>
2024-12-01 18:42:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -33,22 +33,22 @@
 
 <div class="square_images">
     <div class="image_items">
-        <img src="../images/Book1.png" class="images" alt="Books Images" />
+        <img src="../images/book1.png" class="images" alt="Books Images" />
         <p class="image_details">Book: The Alchemist <br /><br /> Moaid55 <br /><br />Location: Jeddah</p>
         <a href="mailto:m45job@outlook.com?subject=Inquiry" style="color:red; font-size: 20px;">Email me</a>
     </div>
     <div class="image_items">
-        <img src="../images/Book2.png" class="images" alt="Books Images" />
+        <img src="../images/book2.png" class="images" alt="Books Images" />
         <p class="image_details">Book: The Old Man and the Sea <br /><br /> Moaid55 <br /><br />Location: Jeddah</p>
         <a href="mailto:m45job@outlook.com?subject=Inquiry" style="color:red; font-size: 20px;">Email me</a>
     </div>
     <div class="image_items">
-        <img src="../images/Book3.png" class="images" alt="Books Images" />
+        <img src="../images/book3.png" class="images" alt="Books Images" />
         <p class="image_details">Book: The Magic Mountain <br /><br /> Moaid55 <br /><br />Location: Jeddah</p>
         <a href="mailto:m45job@outlook.com?subject=Inquiry" style="color:red; font-size: 20px;">Email me</a>
     </div>
     <div class="image_items">
-        <img src="../images/Book4.png" class="images" alt="Books Images" />
+        <img src="../images/book4.png" class="images" alt="Books Images" />
         <p class="image_details">Book: The Stranger <br /><br /> Moaid55 <br /><br />Location: Jeddah</p>
         <a href="mailto:m45job@outlook.com?subject=Inquiry" style="color:red; font-size: 20px;">Email me</a>
     </div>
