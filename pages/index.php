0a76f83bc040f06d545cb441456fa4eedb37852a
Moaid Ali Alshamrani <moaid.alshamrani@gmail.com>
2024-12-01 17:08:39 +0300

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,4 @@
-<?php include('../includes/header.php'); ?>
+<?php include('header.php'); ?>
 
 
 
@@ -60,4 +60,4 @@
 </div>
 
 
-<?php include('../includes/footer.php'); ?>
+<?php include('footer.php'); ?>
