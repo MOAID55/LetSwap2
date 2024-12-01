4cd4ba9210b025ff2c74229b6034417dcb8f5b48
Moaid Ali Alshamrani <moaid.alshamrani@gmail.com>
2024-12-01 17:18:38 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div class="footer" id ="footer">
+        <hr />
+        <p style="text-align: center;">&copy; 2024 Moaid Ali Alshamrani. All rights reserved.</p>
+
+        <a href="https://validator.w3.org/check?uri=http://localhost:3000/pages/">
+            <img src="http://www.w3.org/Icons/valid-xhtml10" alt="HTML Validation" style="height: 25px;" />
+        </a>
+
+        <a href="https://jigsaw.w3.org/css-validator/check/referer">
+            <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Validation" style="height: 25px;" />
+        </a>
+    </div>
+</body>
+</html>
