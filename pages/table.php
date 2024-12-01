8ac88675413681ec5058221f67b85a87f31d4639
Moaid Ali Alshamrani <moaid.alshamrani@gmail.com>
2024-12-01 17:30:23 +0300

    Update table.php

--- pages/table.php
+++ pages/table.php
@@ -1,6 +1,6 @@
 <?php
 $pageTitle = "Table - Page";
-include('../includes/header.php');
+include('./includes/header.php');
 ?>
 
 
@@ -77,4 +77,4 @@ include('../includes/header.php');
 
 
 
-<?php include('../includes/footer.php'); ?>
+<?php include('./includes/footer.php'); ?>
