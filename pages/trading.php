8910e51cf6024d521ff51c9a25ccbf458124bdd4
Moaid Ali Alshamrani <moaid.alshamrani@gmail.com>
2024-12-04 07:18:33 +0300

    Update trading.php

--- pages/trading.php
+++ pages/trading.php
@@ -79,7 +79,7 @@ if (mysqli_num_rows($result) > 0) {
         $bookname = htmlspecialchars($row['bookname']);
         $file = htmlspecialchars($row['file']);
         $Location = htmlspecialchars($row['location']);
-        $image_path = "../k/" . $file;
+        $image_path = "../images/" . $file;
         $name = htmlspecialchars($row['name']);
         $email = htmlspecialchars($row['email']);
 
